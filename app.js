5563f7bbea342b577d96f47b2cede93bdb1ddd43
Jackson <jacksonwhiting00@gmail.com>
2023-02-09 10:13:26 -0500

    modified code to prevent required invalid message from apperaing on startup

--- app.js
+++ app.js
@@ -3,26 +3,33 @@ const warnIcon = document.querySelector("#js-warn-icon");
 const emailAlert = document.querySelector("#js-email-alert");
 const emailSubmitBtn = document.querySelector('#js-email-submit');
 
-const clearMessages = (emailAlert, warnIcon) => {
+//Clears any warnings present
+const clearInvalidMessages = (emailAlert, warnIcon) => {
    emailAlert.innerText = "";
    warnIcon.classList.add("js-hide");
+   emailAddr.classList.remove("js-email-invalid");
 };
 
+//Displays a warning icon and warning message
 const showInvalidMessages = (emailAlert, warnIcon) => {
    emailAlert.innerText = "Please provide a valid email";
    warnIcon.classList.remove("js-hide");
 };
 
+// Validates e-mail address using pattern REGEX on html. If pattern
+// mismatch, validity.valid property will be false
 const validateEmail = (emailAddr) => {
       return emailAddr.validity.valid;
 };
 
+//When user clicks the submit button, clears messages, validates e-mail and shows invalid messages
 const handleEmailInput = (emailAlert, warnIcon, emailAddr) => {
    emailSubmitBtn.addEventListener("click", (e) => {
-      clearMessages(emailAlert, warnIcon);
+      clearInvalidMessages(emailAlert, warnIcon);
       if (!validateEmail(emailAddr)) {
          e.preventDefault();
          showInvalidMessages(emailAlert, warnIcon);
+         emailAddr.classList.add("js-email-invalid");
       }
    })
 }
